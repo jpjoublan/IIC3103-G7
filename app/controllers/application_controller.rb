87a12bfa8e852aa2ccc32107e6650094a146f8c4
Joaquin <jtagle2@uc.cl>
2019-05-29 03:31:52 -0400

    new puts for debuging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -231,7 +231,7 @@ class ApplicationController < ActionController::Base
     	puts 'PIDIENDO PRODUCTOS DENTRO DE FUNCION', sku, almacen_id
         auth_hash = getHash('GET', almacen_id + sku)
         resp = httpGetRequest(BaseURL + 'stock?almacenId=%s&sku=%s&limit=%s' % [almacen_id, sku, limit] , auth_hash)
-        puts 'SE HIZO EL GET'
+        puts 'SE HIZO EL GET', resp
         return resp
     end
 
